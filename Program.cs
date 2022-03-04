70b5b161c427459e7f17de38da48e060b03ccab6
mabbuprem <mabbupremkumar@gmail.com>
2022-03-04 18:58:46 +0530

    numeric password

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace User_Registration_Regex_Unit_Testing
             string[] checkingLastNames = { "Kumar", "Kumar" };
             string[] checkingEMails = { "abc@bl.co", "abc.xyz@bl.co", "abc.xyz@bl.co.in", "abc.@bl.co" };
             string[] checkingNo = { "91 7981587635", "917730002849", "910720659414", "9172065941491" };
-            string[] checkingPassword = { "A222hvhv2626", "917206594149A", "910720659414", "91Ab0659c1491" };
+            string[] checkingPassword = { "Abdvhvhvsbs", "917206594149A", "vvvccbhcbh8", "hcvxhvzA8" };
             userRegistrationPattern.ValidateFirstName(checkingFirstNames);
             userRegistrationPattern.ValidatelastName(checkingLastNames);
             userRegistrationPattern.ValidateeMail(checkingEMails);
